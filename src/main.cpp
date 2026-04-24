efbc01b1a7fc8ed7a4893eb031d7effe49b71793
Diego-DelRio <a25110271@ceti.mx>
2026-04-24 01:14:44 +0000

    Practica 3

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,15 @@
 #include <iostream>
-#include <cstdlib>
+#include <Foco.hpp>
 
 int main(int argc, char* argv[]) {
-    return EXIT_SUCCESS;
+    Foco foquito;
+
+    foquito.Encender();
+    foquito.Apagar();
+
+    std::cout
+    << "Estado Foco: "
+    << foquito.LeerEstado()
+    <<std::endl;
+    return 0;
 }
